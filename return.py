011ebb8710854ec2287b14e7a1f719d5b4d5ba38
ntcbryan <bryanrodrigues899@gmail.com>
2022-12-30 11:06:17 -0300

    aula sobre return

---
+++ return.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Curso luiz otavio
+
+Aula sobre return
+-110
+
+"""
+#Isso não funcionaria, pq vamos comprar a soma1 e soma2, então precisamos do valor "return", o pritn basicamente
+#só printa, o return guarda informação e termina a função
+#def soma:
+#    print(x + y)
+
+def soma(x,y):
+    return (x + y)
+    
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+print(soma1 + soma2)
